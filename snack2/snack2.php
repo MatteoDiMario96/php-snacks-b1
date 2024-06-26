0fde084506bec1a5f69a34248788c8bb979a33b4
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-26 19:34:01 +0200

    Commenti utili.

--- snack2/snack2.php
+++ snack2/snack2.php
@@ -1,10 +1,8 @@
 <?php
-
+// Dico se la variabile è assegnata dagli il primo valore altrimenti il secondo 
 $userName = $_GET['userName'] ?? '';
 $userEmail = $_GET['userEmail'] ?? '';
 $userAge = $_GET['userAge'] ?? '';
-
-
 ?>
 
 
@@ -18,14 +16,17 @@ $userAge = $_GET['userAge'] ?? '';
 <body>
     <main>
 
-        <form action="./risposta.php" method="GET">
-
+        <form action="./snack2.php" method="GET">
+            
             <?php
                 if(strlen($userName) === 0){
+                    //Quando non hai inserito caratteri
                     echo '<p> Inserisci il tuo nome con almeno tre caratteri</p>';
                 }elseif(strlen($userName) < 3){
+                    //Quando hai inserito meno di 3 caratteri   
                     echo '<p> Metti almeno 3 caratteri </p>';
                 }else{
+                    // Quando hai fatto giusto 
                     echo '';
                 }
             ?>
@@ -36,11 +37,14 @@ $userAge = $_GET['userAge'] ?? '';
 
             <?php
                 if(empty($userEmail)){
+                    // Quando hai inserito nessuna mail 
                     echo '<p> Nessuna mail Inserita </p>';
                 }elseif(stripos($userEmail, '.') && stripos($userEmail, '@')){
-                    echo '<p> Email valida! </p>';
+                    // Quando l'email è giusta 
+                    echo '';
                 }else{
-                    echo '<p>Inserisci un Email valida. </p>';
+                    // Quando l' email è inesatta 
+                    echo '<p>Inserisci un Email valida. Almeno un punto e una @. </p>';
                 }
             ?>
 
@@ -50,10 +54,13 @@ $userAge = $_GET['userAge'] ?? '';
             
             <?php
                 if($userAge === ''){
+                    // Quando la variabile è ugaule a stringa vuota 
                     echo '<p> Devi inserire la tua età </p>';
                 }elseif(is_numeric($userAge) === false){
+                    // Quando non è un numero
                     echo '<p> Puoi inserire solo numeri!';
                 }else{
+                    // Quando è giusta. 
                     echo '';
                 }
             ?>
@@ -65,10 +72,13 @@ $userAge = $_GET['userAge'] ?? '';
         <div>
             <?php
                 if($userName === '' && $userEmail === '' && $userAge === ''){
+                    // Tutti parametri vuoti.
                     echo '';
                 }elseif(strlen($userName) > 3 && stripos($userEmail, '.') && stripos($userEmail, '@') && is_numeric($userAge) === true){
+                    // Tutti parametri giusti.
                     echo 'Accesso consentito';
                 }elseif(strlen($userName) < 3 || stripos($userEmail, '.') === false || stripos($userEmail, '@') === false || is_numeric($userAge) === false){
+                    // Anche un solo parametro sbagliato
                     echo 'Accesso negato';
                 }
             ?>
