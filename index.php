0f4deacd295ffb9b2823182c321fee3498717fd0
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-26 12:35:07 +0200

    Bruttino forse se usavo foreach meglio.

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 $randNumb1 = rand(20, 100);
 $randNumb2 = rand(20, 100);
+$randNumb3 = rand(20, 100);
+$randNumb4 = rand(20, 100);
+$randNumb5 = rand(20, 100);
+$randNumb6 = rand(20, 100);
+
 
 
 $arrayA = [
@@ -14,14 +19,14 @@ $arrayA = [
     [
         'squadraCasa' => 'Olimpia Roma',
         'squadraOspite' => 'Parigi',
-        'puntiFattiCasa' => $randNumb1, 
-        'puntiFattiOspite' => $randNumb2,
+        'puntiFattiCasa' => $randNumb3, 
+        'puntiFattiOspite' => $randNumb4,
     ],
     [
         'squadraCasa' => 'Olimpia Genoa',
         'squadraOspite' => 'Dubai',
-        'puntiFattiCasa' => $randNumb1, 
-        'puntiFattiOspite' => $randNumb2,
+        'puntiFattiCasa' => $randNumb5, 
+        'puntiFattiOspite' => $randNumb6,
     ],
 ];
 
@@ -40,16 +45,63 @@ var_dump($arrayA);
         <h1>
             Calendario:
         </h1>
-        <h2>
-            <?php
-                echo($arrayA[0]['squadraCasa']);
-            ?>
-            -
-            <?php
-                echo($arrayA[0]['squadraOspite']);
-            ?>
-
-        </h2>
+        <article>
+            <h2>
+                <?php
+                    echo($arrayA[0]['squadraCasa']);
+                ?>
+                -
+                <?php
+                    echo($arrayA[0]['squadraOspite']);
+                ?>
+                ||
+                <?php
+                    echo($arrayA[0]['puntiFattiCasa']);
+                ?>
+                -
+                <?php
+                    echo($arrayA[0]['puntiFattiOspite']);
+                ?>
+            </h2>
+        </article>
+        <article>
+            <h2>
+                <?php
+                    echo($arrayA[1]['squadraCasa']);
+                ?>
+                -
+                <?php
+                    echo($arrayA[1]['squadraOspite']);
+                ?>
+                ||
+                <?php
+                    echo($arrayA[1]['puntiFattiCasa']);
+                ?>
+                -
+                <?php
+                    echo($arrayA[1]['puntiFattiOspite']);
+                ?>
+            </h2>
+        </article>
+        <article>
+            <h2>
+                <?php
+                    echo($arrayA[2]['squadraCasa']);
+                ?>
+                -
+                <?php
+                    echo($arrayA[2]['squadraOspite']);
+                ?>
+                ||
+                <?php
+                    echo($arrayA[2]['puntiFattiCasa']);
+                ?>
+                -
+                <?php
+                    echo($arrayA[2]['puntiFattiOspite']);
+                ?>
+            </h2>
+        </article>
     </main>
 </body>
 </html>
