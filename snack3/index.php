1bbe556e1e82c62bad984863a9acbe33fd45975c
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-26 20:02:07 +0200

    Snack 3 completato !

---
+++ snack3/index.php
@@ -0,0 +1,51 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+foreach ($posts as $key => $post) {
+    echo $key . '<br>';
+    foreach ($post as $key => $section) {
+        echo $section['title'] . '<br>';
+        echo $section['author'] . '<br>';
+        echo $section['text'] . '<br>' . '<br>';
+    }
+}
+?>
